cff0c0e62126c20d46fb14836e97c060f0557503
ibrahim-egy <ibrahimtarek.h@gmail.com>
2022-10-21 18:26:21 +0200

    added read more nav button

--- main.py
+++ main.py
@@ -24,53 +24,7 @@ def load_image_into_numpy_array(path):
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-
-@app.route('/result', methods=['GET', 'POST'])
-def result():
-    if request.method == 'POST':
-        image = request.files['img']
-        image_path = f'static/images/{image.filename}'
-        image.save(image_path)
-        image_np = load_image_into_numpy_array(image_path)
-        input_tensor = tf.convert_to_tensor(image_np)
-        input_tensor = input_tensor[tf.newaxis, ...]
-
-        detections = detect_fn(input_tensor)
-        num_detections = int(detections.pop('num_detections'))
-        detections = {key: value[0, :num_detections].numpy() for key, value in detections.items()}
-        detections['num_detections'] = num_detections
-
-        # detection_classes should be ints.
-        detections['detection_classes'] = detections['detection_classes'].astype(np.int64)
-
-        image_np_with_detections = image_np.copy()
-
-        labels_detected = viz_utils.visualize_boxes_and_labels_on_image_array(
-            image_np_with_detections,
-            detections['detection_boxes'],
-            detections['detection_classes'],
-            detections['detection_scores'],
-            category_index,
-            use_normalized_coordinates=True,
-            max_boxes_to_draw=200,
-            min_score_thresh=.4)
-        plt.figure(figsize=IMAGE_SIZE, dpi=200)
-        plt.axis("off")
-        plt.imshow(image_np_with_detections)
-        plt.savefig('static/outputs/result.png', bbox_inches='tight')
-        im = Image.open("static/outputs/result.png")
-
-        # show image
-        im.show()
-        classes = [cls for cls in detections['detection_classes'][detections['detection_scores'] > .4]]
-        classes = [category_index.get(cls)['name'] for cls in classes]
-        data = {}
-        infections = {
+infections = {
             'unhealthy sooty mold': {
                 "treat": "Spray With detergent Mixture",
                 "link": "http://extension.msstate.edu/publications/information-sheets/the-plant-doctor-sooty-mold#:~:text=Once%20sooty%20mold%20is%20established,spray%20it%20on%20the%20plants.",
@@ -97,6 +51,53 @@ def result():
                 'no_treatment': "none"
             }
         }
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/result', methods=['GET', 'POST'])
+def result():
+    if request.method == 'POST':
+        image = request.files['img']
+        image_path = f'static/images/{image.filename}'
+        image.save(image_path)
+        image_np = load_image_into_numpy_array(image_path)
+        input_tensor = tf.convert_to_tensor(image_np)
+        input_tensor = input_tensor[tf.newaxis, ...]
+
+        detections = detect_fn(input_tensor)
+        num_detections = int(detections.pop('num_detections'))
+        detections = {key: value[0, :num_detections].numpy() for key, value in detections.items()}
+        detections['num_detections'] = num_detections
+
+        # detection_classes should be ints.
+        detections['detection_classes'] = detections['detection_classes'].astype(np.int64)
+
+        image_np_with_detections = image_np.copy()
+
+        viz_utils.visualize_boxes_and_labels_on_image_array(
+            image_np_with_detections,
+            detections['detection_boxes'],
+            detections['detection_classes'],
+            detections['detection_scores'],
+            category_index,
+            use_normalized_coordinates=True,
+            max_boxes_to_draw=200,
+            min_score_thresh=.4)
+        plt.figure(figsize=IMAGE_SIZE, dpi=200)
+        plt.axis("off")
+        plt.imshow(image_np_with_detections)
+        plt.savefig('static/outputs/result.png', bbox_inches='tight')
+        im = Image.open("static/outputs/result.png")
+
+        # show image
+        im.show()
+        classes = [cls for cls in detections['detection_classes'][detections['detection_scores'] > .4]]
+        classes = [category_index.get(cls)['name'] for cls in classes]
+        data = {}
+
         for i in range(len(classes)):
             name = f"{i+1}_{classes[i]}"
             score = detections['detection_scores'][i] * 100
